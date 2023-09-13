d795031367170c792bb3fa696c0df3729fd3be69
Hoaper <beeboplay@gmail.com>
2023-09-13 22:05:39 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import pickle
+from sklearn.preprocessing import MinMaxScaler
+
+app = Flask("Churn Prediction")
+
+
+numerical = [
+    'tenure', 
+    'monthlycharges', 
+    'totalcharges'
+]
+
+categorical = [
+    'gender',
+    'seniorcitizen',
+    'partner',
+    'dependents',
+    'phoneservice',
+    'multiplelines',
+    'internetservice',
+    'onlinesecurity',
+    'onlinebackup',
+    'deviceprotection',
+    'techsupport',
+    'streamingtv',
+    'streamingmovies',
+    'contract',
+    'paperlessbilling',
+    'paymentmethod'
+]
+
+le_enc_cols = ['gender', 'partner', 'dependents','paperlessbilling', 'phoneservice']
+gender_map = {'male': 0, 'female': 1}
+y_n_map = {'yes': 1, 'no': 0}
+
+# Логистическая модель
+model_file_path = "models/lr_model_churn_prediction.sav"
+model = pickle.load(open(model_file_path, 'rb'))
+
+encoding_model_file_path = "models/lr_model_churn_prediction_encode.sav"
+encoding_model = pickle.load(open(encoding_model_file_path, 'rb'))
+
+treshold = 0.281
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    customer = request.get_json()
+    data = pd.DataFrame(customer, index=[0])
+
+    scaler = MinMaxScaler()
+
+    df_copy = data.copy()
+    df_copy[numerical] = scaler.fit_transform(df_copy[numerical])
+
+    for col in le_enc_cols:
+        if col == 'gender':
+            df_copy[col] = df_copy[col].map(gender_map)
+        else:
+            df_copy[col] = df_copy[col].map(y_n_map)
+
+    dicts_df = df_copy[categorical + numerical].to_dict(orient='records')
+    X = encoding_model.transform(dicts_df)
+    y_pred = model.predict_proba(X)[:, 1]
+    churn_descision = (y_pred >= treshold)
+
+    result = {
+        "churn_descision": bool(churn_descision[0]),
+        "churn_probability": float(y_pred[0])
+    }
+
+    return jsonify(result)
+
+
+app.run(debug=True)
